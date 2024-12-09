f9ec9bc6dbdab3adff171d801f1e4b4a1cc737e0
InterMaus1154 <amarco1154@gmail.com>
2024-12-09 19:46:32 +0000

    session class init

---
+++ core/Session.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Core;
+
+class Session extends Singleton
+{
+    protected function __construct()
+    {
+        parent::__construct();
+    }
+
+
+}
--- index.php
+++ index.php
@@ -7,16 +7,19 @@ require "core/App.php";
 require "core/Router.php";
 require "core/Route.php";
 require "core/Redirect.php";
+require "core/Session.php";
 
 use Core\App;
 use Core\Router;
 use Core\Redirect;
+use Core\Session;
 
 $app = App::getInstance();
 
 $app->registerService('router', Router::getInstance());
 $app->registerService('routes', require "routes/web.php");
 $app->registerService('redirect', Redirect::getInstance());
+$app->registerService('session', Session::getInstance());
 
 $app->run();
 
