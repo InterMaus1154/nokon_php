84633705b441a0f2e0acb4091038d56c10553a46
intermaus1154 <amarco1154@gmail.com>
2025-04-23 08:03:41 +0200

    File clean up

--- user/routes.php
+++ user/routes.php
@@ -6,10 +6,7 @@ use user\app\controllers\ViewController;
 return new class extends RouteStorage {
     public function registerRoutes(): self
     {
-        $this->get('/', fn() => view('index'));
 
-        $this->get('/test', [ViewController::class, 'index']);
-        $this->get('/about', [ViewController::class, 'about']);
 
         return $this;
     }
