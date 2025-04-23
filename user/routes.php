396464e0737a6d0a17a5c59ce6cdd4cda8fac061
intermaus1154 <amarco1154@gmail.com>
2025-04-23 08:05:37 +0200

    add comments to routes.php about defining routes

--- user/routes.php
+++ user/routes.php
@@ -1,12 +1,14 @@
 <?php
 
 use core\routing\RouteStorage;
-use user\app\controllers\ViewController;
+
+// You can define your own routes in the method below
+// Do not remove the return statement at the end of the method
 
 return new class extends RouteStorage {
+
     public function registerRoutes(): self
     {
-
 
         return $this;
     }
